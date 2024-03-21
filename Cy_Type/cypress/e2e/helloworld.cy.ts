1e72c63a51d2d60d245962606c696c4ba0dd5d33
Aaron <158441796+Aaron-Initialyze@users.noreply.github.com>
2024-03-21 09:48:41 +0530

    Update helloworld.cy.ts
    added a failing assertion

--- Cy_Type/cypress/e2e/helloworld.cy.ts
+++ Cy_Type/cypress/e2e/helloworld.cy.ts
@@ -2,6 +2,7 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('/who-we-are')
+    cy.get("button.cm-btn.cm-btn-success.cm-btn-accept-all").should('contain','QAClick Academy');
     cy.get("button.cm-btn.cm-btn-success.cm-btn-accept-all").click()
   })
   it('output', ()=>{
